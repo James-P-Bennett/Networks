d30881c317190ef862247e5eaa84356e99d0f432
James-P-Bennett <james.ph.bennett@gmail.com>
2024-08-26 13:16:48 -0500

    Update MainFlexGroup.java

--- src/main/java/io/github/sefiraat/networks/slimefun/groups/MainFlexGroup.java
+++ src/main/java/io/github/sefiraat/networks/slimefun/groups/MainFlexGroup.java
@@ -61,7 +61,7 @@ public class MainFlexGroup extends FlexItemGroup {
     @Override
     @ParametersAreNonnullByDefault
     public void open(Player p, PlayerProfile profile, SlimefunGuideMode mode) {
-        final ChestMenu chestMenu = new ChestMenu(Theme.MAIN.getColor() + "Networks");
+        final ChestMenu chestMenu = new ChestMenu("&f七七七七七七七七⎬");
 
         for (int slot : HEADER) {
             chestMenu.addItem(slot, ChestMenuUtils.getBackground(), (player1, i1, itemStack, clickAction) -> false);
